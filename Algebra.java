84a0744a7c6b18b32595bfaf8194300437106cba
benkarmi <69310049+benkarmi@users.noreply.github.com>
2025-11-21 10:33:29 +0200

    updateD

--- Algebra.java
+++ Algebra.java
@@ -4,94 +4,96 @@
 // return int values.
 
 public class Algebra {
-	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(100));
-	}  
+    public static void main(String args[]) {
+        // Tests some of the operations
+        System.out.println(plus(2,3));   // 2 + 3
+        System.out.println(minus(7,2));  // 7 - 2
+        System.out.println(minus(2,7));  // 2 - 7
+        System.out.println(times(3,4));  // 3 * 4
+        System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+        System.out.println(pow(5,3));      // 5^3
+        System.out.println(pow(3,5));      // 3^5
+        System.out.println(div(12,3));   // 12 / 3    
+        System.out.println(div(5,5));    // 5 / 5  
+        System.out.println(div(25,7));   // 25 / 7
+        System.out.println(mod(25,7));   // 25 % 7
+        System.out.println(mod(120,6));  // 120 % 6    
+        System.out.println(sqrt(36));
+        System.out.println(sqrt(263169));
+        System.out.println(sqrt(100));
+    }  
 
-	// Returns x1 + x2
-	public static int plus(int x1, int x2) {
-		while (x1 > 0) {
-			x2++;
-			x1--;
-		}
-		while (x1 < 0) {
-			x2--;
-			x1++;
-		}
-	return x2;
-	}
+    // Returns x1 + x2
+    public static int plus(int x1, int x2) {
+        while (x1 > 0) {
+            x2++;
+            x1--;
+        }
+        while (x1 < 0) {
+            x2--;
+            x1++;
+        }
+        return x2;
+    }
 
-	// Returns x1 - x2
-	public static int minus(int x1, int x2) {
-		if (x2 != 0) {
-			while (x2 > 0 ) {
-				x1--;
-				x2--;
-			}
-			while (x2 < 0 ) {
-				x1++;
-				x2++;
-			}
-		}
-		return x1;
-	}
+    // Returns x1 - x2
+    public static int minus(int x1, int x2) {
+        while (x2 > 0 ) {
+            x1--;
+            x2--;
+        }
+        while (x2 < 0 ) {
+            x1++;
+            x2++;
+        }
+        return x1;
+    }
 
-	// Returns x1 * x2
-	public static int times(int x1, int x2) {
-		int sumTimes = 0;
-		for (int i = 0; i<x2; i++) {
-			sumTimes = plus(sumTimes, x1);
-		}
-	return sumTimes;
-	}
+    // Returns x1 * x2  (assumes x2 >= 0 as in the tests)
+    public static int times(int x1, int x2) {
+        int sumTimes = 0;
+        for (int i = 0; i < x2; i++) {
+            sumTimes = plus(sumTimes, x1);
+        }
+        return sumTimes;
+    }
 
-	// Returns x^n (for n >= 0)
-	public static int pow(int x, int n) {
-		int result = 1;
-		for (int i = 0; i < n; i++) {
-			result = times(result, x);
-		}
-	return result;
-	}
+    // Returns x^n (for n >= 0)
+    public static int pow(int x, int n) {
+        int result = 1;
+        for (int i = 0; i < n; i++) {
+            result = times(result, x);
+        }
+        return result;
+    }
 
-	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		int countDiv = 0;
-		for (int i = 0; i < x1; i = plus(i, x2)) {
-			countDiv = plus(countDiv, 1);
-		}
-	return countDiv;
-	}
+    // Returns the integer part of x1 / x2  (i.e., floor division)
+    public static int div(int x1, int x2) {
+        int countDiv = 0;
+        // repeatedly subtract x2 from x1 while we still can
+        while (x1 >= x2) {
+            x1 = minus(x1, x2);       // x1 = x1 - x2
+            countDiv = plus(countDiv, 1);  // countDiv++
+        }
+        return countDiv;
+    }
 
-	// Returns x1 % x2
-	public static int mod(int x1, int x2) {
-		while (x1 >= x2) {
-			x1 = minus(x1, x2);
-		}
-		return x1;
-	}	
+    // Returns x1 % x2
+    public static int mod(int x1, int x2) {
+        while (x1 >= x2) {
+            x1 = minus(x1, x2);
+        }
+        return x1;
+    }   
 
-	// Returns the integer part of sqrt(x) 
-	public static int sqrt(int x) {
-		int i = 0;
-		while (times(i, i) < x) {
-			i++;
-		}
-	return i;
-	}	  	  
+    // Returns the integer part (floor) of sqrt(x) 
+    public static int sqrt(int x) {
+        int i = 0;
+        // find the smallest i such that i^2 > x
+        while (times(i, i) <= x) {
+            i++;
+        }
+        // step back one: (i-1)^2 <= x < i^2
+        return minus(i, 1);
+    }       
 }
